cd0193d3bd43c24e86eb3a1a4bf129e9ca6e8580
Alli Michaelis <allison.michaelis@gmail.com>
2016-05-19 21:12:17 -0500

    Alli Test Update

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>Time Tracker</h1>'
+  template: '<h1>Alli is the best girlfriend EVER!</h1>'
 })
 export class AppComponent {
   
